21a9dcfeda91b53550943cc3d051799caf8694fc
dgrossman <dgrossman@ForbinProject.local>
2017-02-05 10:58:11 -0800

    moved F_IT, added strings

--- verboten_words/verboten_words.py
+++ verboten_words/verboten_words.py
@@ -6,35 +6,41 @@ import os
 import sys
 
 
-def findBad(file_obj):
-    badWords = list()
+def find_bad(bad_words, file_obj):
+    """ search a file for any bad_words
+    if no bad words found return (0,'')
+    if bad words found return (1,'lines locations')
 
-    vFilename = os.environ.get('VERBOTEN_FILE')
+    Attributes:
 
-    if vFilename is not None:
-        try:
-            vFile = open(vFilename, 'r')
-            for line in vFile:
-                line = line.strip()
-                if not line.startswith('#'):
-                    badWords.append(line)
-            vFile.close()
-        except:
-            pass
+        bad_words(list): words not to appear in repo
+        file_obj(fd): filedescriptor to search
 
+    """
     for idx, line in enumerate(file_obj):
-        for word in badWords:
+        bad_found = False
+        msg = ''
+        for word in bad_words:
             if line.find(word) >= 0:
-                msg = 'Line:{0} Word:{1}'.format(idx, word)
-                return (1, msg)
-
-    return (0, None)
+                bad_found = True
+                msg += 'Line:{0} Word:{1}\n'.format(idx, word)
+    return (bad_found, msg)
 
 
 def verboten_words(argv=None):
+    """ entry point for the module
+
+    ENVIRONEMNT_VARIABLES:
+
+    VERBOTEN_FILE: pointer to file of bad_words
+    F_IT: Existance of F_IT negates all checking for bad words
+    
+    returnValue of 0 -> all good/ignored, 1 -> problems
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to fix')
     args = parser.parse_args(argv)
+    bad_words = list()
 
     retv = 0
 
@@ -44,16 +50,29 @@ def verboten_words(argv=None):
     except KeyError:
         pass
 
+    v_filename = os.environ.get('VERBOTEN_FILE')
+
+    if v_filename is not None:
+        try:
+            v_file = open(v_filename, 'r')
+            for line in v_file:
+                line = line.strip()
+                if not line.startswith('#'):
+                    bad_words.append(line)
+            v_file.close()
+
+        except:
+            pass
+
     for filename in args.filenames:
         # Read as binary so we can read byte-by-byte
         with open(filename, 'r') as file_obj:
-            ret_for_file, word = findBad(file_obj)
+            ret_for_file, word = find_bad(bad_words, file_obj)
             if ret_for_file:
                 print('File:{0} {1}'.format(filename, word))
             retv |= ret_for_file
 
     return retv
-
 
 if __name__ == '__main__':
     sys.exit(verboten_words())
